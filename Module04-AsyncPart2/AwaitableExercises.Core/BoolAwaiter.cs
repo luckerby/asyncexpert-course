f9d9066bb531555853f75d0eed4d533adbff1065
Mihai Albert <malbert@temenos.com>
2020-07-12 22:47:39 +0300

    Add extension method for bool and uncomment tests

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -5,13 +5,23 @@ namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        // the Bool type needs to be extended to support the OnCompleted method defined by INotifyCompletion
+        public static BoolAwaiter GetAwaiter(this bool boolVar)
+        {
+            return new BoolAwaiter(boolVar);
+        }
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
-        public void OnCompleted(Action continuation)
+        private readonly bool _boolVar = false;
+        public void OnCompleted(Action continuation) => continuation();
+        public bool IsCompleted => true;
+        public bool GetResult() => _boolVar;
+
+        public BoolAwaiter(bool pBoolVar)
         {
-            throw new NotImplementedException();
+            _boolVar = pBoolVar;
         }
     }
 }
