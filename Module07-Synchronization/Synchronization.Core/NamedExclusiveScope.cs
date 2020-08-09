0a879cf4bf866ca680863e036175408084887dc9
Mihai Albert <malbert@temenos.com>
2020-08-09 19:15:00 +0300

    Make tests pass for the Synchronization module

--- Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
+++ Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Synchronization.Core
@@ -9,10 +10,37 @@ namespace Synchronization.Core
      *
      * Created region may be system-wide or not, depending on the constructor parameter.
      */
+
+    // There's a requirement for system-wide as well, so SemaphoreSlim is out
+    // of the question.
     public class NamedExclusiveScope : IDisposable
     {
+        private Semaphore _semaphore;
+
         public NamedExclusiveScope(string name, bool isSystemWide)
         {
+            if (!isSystemWide)
+            {
+                // Append something particular to this process only. The PID will do
+                name += $"-{Process.GetCurrentProcess().Id}";
+            }
+
+            _semaphore = new Semaphore(1, 1, name, out var newSemaphoreCreated);
+            if (!newSemaphoreCreated)
+            {
+                // If we're here, then it means that a semaphore with the intended
+                //  name already exists
+                throw (new System.InvalidOperationException($"Unable to get a global lock {name}."));
+            }
+            
+            // Guard access to the critical section
+            _semaphore.WaitOne();
+        }
+
+        public void Dispose()
+        {
+            // It's here that we need to release whatever lock we've used
+            _semaphore.Release();
         }
     }
 }
--- Module07-Synchronization/Synchronization/Program.cs
+++ Module07-Synchronization/Synchronization/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using Synchronization.Core;
 
@@ -15,10 +16,29 @@ namespace Synchronization
                 scopeName = args[0];
                 isSystemWide = bool.Parse(args[1]);
             }
+
+            /*
+            Thread thread1 = new Thread(MyMethod);
+            Thread thread2 = new Thread(MyMethod);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            */
+
             using (new NamedExclusiveScope(scopeName, isSystemWide))
             {
                 Console.WriteLine("Hello world!");
                 Thread.Sleep(300);
+            }
+        }
+
+        static void MyMethod()
+        {
+            using (new NamedExclusiveScope("default", false))
+            {
+                Console.WriteLine($"Hello world from thread: {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(3000);
             }
         }
     }
